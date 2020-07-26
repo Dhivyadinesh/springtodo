fa46b7a6a862b94f256a081ec497ff9fbc4ec95c
dinesh <dinesh@dinesh-PC>
2020-07-26 22:33:29 +0530

    commit

---
+++ springtodo/README.md
@@ -0,0 +1,1 @@
+# This to create a user login page and register page for creating an todolist in which add,edit and delete the event.
---
+++ springtodo/src/main/java/com/dd/repository/TodoRepository.java
@@ -0,0 +1,13 @@
+package com.dd.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dd.entity.Todo;
+
+
+public interface TodoRepository extends JpaRepository<Todo, Long>{
+	List<Todo> findByUserName(String user);
+}
+
